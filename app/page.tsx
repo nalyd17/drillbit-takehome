8a092671b9be2bb114570992412fbe344217438a
nalyd17 <dylan.p.brown@gmail.com>
2025-08-07 21:51:38 -0500

    Adopt (roughly) Drillbit color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-5xl font-bold">Drillbit Takehome</h1>
+    <div className="bg-background flex min-h-screen items-center justify-center">
+      <h1 className="bg-drillbit-orange rounded-lg p-4 text-5xl font-bold text-white">
+        Drillbit Takehome
+      </h1>
+      <h1 className="bg-drillbit-gray rounded-lg p-4 text-5xl font-bold text-white">
+        Drillbit Takehome
+      </h1>
     </div>
   );
 }
